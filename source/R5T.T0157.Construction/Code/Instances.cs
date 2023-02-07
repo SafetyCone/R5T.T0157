96b8787268c18d3e09f42f792367944ed008350f
davidcoats <david@getsafetycone.com>
2023-02-07 12:27:46 -0800

    Initial commit.

---
+++ source/R5T.T0157.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0157.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.T0157/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0157
+{
+	/// <summary>
+	/// CssSociety and other HTML-related strong types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
